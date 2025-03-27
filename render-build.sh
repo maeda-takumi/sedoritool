d289b439cfaec3fbcaf1edcda0372ce5a6f3033e
maeda-takumi <jiangqiantian54@gmail.com>
2025-03-27 15:25:42 +0900

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-# Chromium をインストール（必要な場合）
+# 必要なライブラリをインストール (Chromiumに必要なライブラリ)
 apt-get update
-apt-get install -y chromium-browser
+apt-get install -y \
+    chromium \
+    chromium-driver \
+    libxss1 \
+    libappindicator3-1 \
+    libgdk-pixbuf2.0-0 \
+    libnss3 \
+    libasound2 \
+    fonts-liberation \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libcups2 \
+    libx11-xcb1 \
+    libnspr4 \
+    libnss3-dev \
+    libxcomposite1 \
+    libxrandr2 \
+    xdg-utils \
+    libgtk-3-0
 
-# 必要な依存関係をインストール（もし必要なら）
-apt-get install -y libxss1 libappindicator3-1 libindicator7
+# 他のインストール処理が必要なら追加
