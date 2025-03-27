2ed2adf6d386dec52791930c6a2b4d601a136fa0
maeda-takumi <jiangqiantian54@gmail.com>
2025-03-27 18:34:42 +0900

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# パッケージリストを更新
+# Chromium用の依存パッケージをインストール
 apt-get update
-
-# 必要なパッケージをインストール
 apt-get install -y libgtk-4-1 libgraphene-1.0-0 libgstgl-1.0-0 libgstcodecparsers-1.0-0 libavif15 libenchant-2-2 libsecret-1-0 libmanette-0-2 libglesv2-2
 
-# Playwrightをインストール
-pip install playwright
-playwright install
+# Seleniumをインストール
+pip install selenium
 
 # Pythonパッケージのインストール（requirements.txtに依存関係が含まれている場合）
 pip install -r requirements.txt
