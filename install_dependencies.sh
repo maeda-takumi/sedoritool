7296e99751ef5384eddd7832c16ec46b35987d0e
maeda-takumi <jiangqiantian54@gmail.com>
2025-03-27 18:24:51 +0900

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-# パッケージリストの更新
-echo "Updating package lists..."
-sudo apt-get update
+# パッケージリストを更新
+apt-get update
 
-# 必要なパッケージのインストール
-echo "Installing necessary packages..."
-sudo apt-get install -y \
-  libgtk-4-1 \
-  libgraphene-1.0-0 \
-  libgstgl-1.0-0 \
-  libgstcodecparsers-1.0-0 \
-  libavif15 \
-  libenchant-2-2 \
-  libsecret-1-0 \
-  libmanette-0.2-0 \
-  libgles2
+# 必要なパッケージをインストール
+apt-get install -y libgtk-4-1 libgraphene-1.0-0 libgstgl-1.0-0 libgstcodecparsers-1.0-0 libavif15 libenchant-2-2 libsecret-1-0 libmanette-0-2 libglesv2-2
 
-# Playwrightのインストール
-echo "Installing Playwright dependencies..."
+# Playwrightをインストール
+pip install playwright
 playwright install
-
-echo "Installation completed!"
