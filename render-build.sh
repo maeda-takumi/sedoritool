030a180cb717e9b05ccefe7103a07cf98f257c44
maeda-takumi <jiangqiantian54@gmail.com>
2025-03-27 15:20:56 +0900

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Google Chrome を手動でインストール
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# Chromium をインストール（必要な場合）
+apt-get update
+apt-get install -y chromium-browser
 
 # 必要な依存関係をインストール（もし必要なら）
-apt-get update
 apt-get install -y libxss1 libappindicator3-1 libindicator7
-
-# ダウンロードした Chrome をインストール
-dpkg -i google-chrome-stable_current_amd64.deb
-apt-get -f install  # 依存関係を解決
