c38b6c5861b4da7dadd45c6ff2fa0dd0060465bc
maeda-takumi <jiangqiantian54@gmail.com>
2025-03-28 18:51:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,10 @@ import os
 import urllib.request
 import zipfile
 from flask import Flask, jsonify
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 import traceback
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024  # 最大200MB
-
-chrome_driver_path = "/tmp/chromedriver-linux64/chromedriver"  # 解凍先のパス
-chrome_path = "/tmp/chrome-linux64/chrome"  # 解凍先のパス
 
 # ダウンロードと解凍先のディレクトリ
 tmp_dir = "/tmp"
@@ -52,40 +46,6 @@ def install_chrome():
         # 不要なzipファイルを削除
         os.remove(chrome_zip_path)
         os.remove(chromedriver_zip_path)
-
-        # WebDriverの作成関数
-        def create_webdriver():
-            try:
-                # Chromeのオプション設定
-                chrome_options = Options()
-                chrome_options.binary_location = chrome_path  # Chromeのバイナリ位置を設定
-                chrome_options.add_argument('--headless')  # ヘッドレスモードで起動
-                chrome_options.add_argument('--no-sandbox')  # セキュリティ回避
-                chrome_options.add_argument('--disable-dev-shm-usage')  # システムリソース回避
-        
-                # Chromeドライバサービスを設定
-                service = Service(executable_path=chrome_driver_path)
-        
-                # WebDriverの作成
-                driver = webdriver.Chrome(service=service, options=chrome_options)
-                print("driverを取得しました")  # ここでメッセージを表示
-                return driver
-            except Exception as e:
-                # WebDriver作成失敗時のエラー処理
-                print(f"Error creating WebDriver: {e}")
-                print("WebDriver作成中にエラーが発生しました:")
-                print(traceback.format_exc())  # 詳細なエラーメッセージを表示
-
-                # chromeとdriverの展開先のリストを表示
-                print(f"Chrome展開先: {chrome_extract_dir} 中身: {os.listdir(chrome_extract_dir)}")
-                print(f"ChromeDriver展開先: {chromedriver_extract_dir} 中身: {os.listdir(chromedriver_extract_dir)}")
-                
-                return None
-
-        # WebDriverを作成
-        driver = create_webdriver()
-        if driver is None:
-            return jsonify({"error": "WebDriverの作成に失敗しました"}), 500
 
         # 返すJSONの内容
         return jsonify({
